1c9a589db49ee51e78fbfa907e76cec556692c0b
Jobert Poylan Jr <jobertdev27@gmail.com>
2025-09-06 14:39:23 +0800

    init: added mobile and styling

---
+++ mobile-app/app/(tabs)/_layout.tsx
@@ -0,0 +1,69 @@
+import React from "react";
+import FontAwesome from "@expo/vector-icons/FontAwesome";
+import { Link, Tabs } from "expo-router";
+import { Pressable } from "react-native";
+
+import Colors from "@/constants/Colors";
+import { useColorScheme } from "@/components/useColorScheme";
+import { useClientOnlyValue } from "@/components/useClientOnlyValue";
+
+// You can explore the built-in icon families and icons on the web at https://icons.expo.fyi/
+function TabBarIcon(props: {
+  name: React.ComponentProps<typeof FontAwesome>["name"];
+  color: string;
+}) {
+  return <FontAwesome size={24} style={{ marginBottom: -3 }} {...props} />;
+}
+
+export default function TabLayout() {
+  const colorScheme = useColorScheme();
+
+  return (
+    <Tabs
+      screenOptions={{
+        tabBarActiveTintColor: Colors[colorScheme ?? "light"].tint,
+        // Disable the static render of the header on web
+        // to prevent a hydration error in React Navigation v6.
+        headerShown: useClientOnlyValue(false, true),
+        headerStyle: {
+          backgroundColor: Colors[colorScheme ?? "light"].darker,
+        },
+        tabBarStyle: {
+          backgroundColor: Colors[colorScheme ?? "light"].darker,
+          margin: 50,
+        },
+      }}
+    >
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: "Library",
+          tabBarIcon: ({ color }) => (
+            <TabBarIcon name="th-list" color={color} />
+          ),
+          headerRight: () => (
+            <Link href="/modal" asChild>
+              <Pressable>
+                {({ pressed }) => (
+                  <FontAwesome
+                    name="info-circle"
+                    size={25}
+                    color={Colors[colorScheme ?? "light"].text}
+                    style={{ marginRight: 15, opacity: pressed ? 0.5 : 1 }}
+                  />
+                )}
+              </Pressable>
+            </Link>
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="two"
+        options={{
+          title: "Browse",
+          tabBarIcon: ({ color }) => <TabBarIcon name="search" color={color} />,
+        }}
+      />
+    </Tabs>
+  );
+}
---
+++ mobile-app/constants/Colors.ts
@@ -0,0 +1,21 @@
+const tintColorLight = "#2f95dc";
+const tintColorDark = "#ed8796";
+
+export default {
+  light: {
+    text: "#000",
+    background: "#fff",
+    darker: "#181926",
+    tint: tintColorLight,
+    tabIconDefault: "#ccc",
+    tabIconSelected: tintColorLight,
+  },
+  dark: {
+    text: "#cad3f5",
+    background: "#1e2030",
+    darker: "#181926",
+    tint: tintColorDark,
+    tabIconDefault: "#ccc",
+    tabIconSelected: tintColorDark,
+  },
+};
