54ae541ec683dc544520164b986b38e36829f921
Jobert Poylan Jr <jobertdev27@gmail.com>
2025-09-25 17:31:12 +0800

    feat: added color palette

---
+++ frontend/src/components/Header.tsx
@@ -0,0 +1,10 @@
+export default function Header() {
+  return (
+    <header>
+      <div>
+        <h1 id="header-title">Yomutsugi</h1>
+      </div>
+      <div id="header-nav"></div>
+    </header>
+  );
+}
