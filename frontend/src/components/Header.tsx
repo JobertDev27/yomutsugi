8b38380593d2f20729d461cc0b14fbd4a512c205
Jobert Poylan Jr <jobertdev27@gmail.com>
2025-10-04 11:34:18 +0800

    feat: added polish to header

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -3,8 +3,15 @@ export default function Header() {
     <header>
       <div id="header-title-cont">
         <h1>Yomutsugi</h1>
+        <nav className="header-nav">
+          <button className="search-btn">
+            <img src="../public/search.png" alt="" />
+          </button>
+          <a href="">LIBRARY</a>
+          <a href="">TIER LIST</a>
+          <button className="important-btn">SIGN OUT</button>
+        </nav>
       </div>
-      <div className="header-nav"></div>
     </header>
   );
 }
