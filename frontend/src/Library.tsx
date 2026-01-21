cd37d62f33904edfb92e62b5932d73a9f8cfd50a
Jobert Poylan Jr <jobertdev27@gmail.com>
2026-01-21 19:33:57 +0800

    feat: added database rendering in library

--- frontend/src/Library.tsx
+++ frontend/src/Library.tsx
@@ -21,23 +21,21 @@ const supabase = createClient(
 );
 
 export default function Library() {
-  const [animeList, setAnimeList] = useState<CardProp | null>(null);
+  const [animeList, setAnimeList] = useState<any[] | null>(null);
   const userId = useContext(sessionContext)?.user?.id;
 
   useEffect(() => {
     const fetch_data = async () => {
-      const res = await get_anime_by_id("27");
-      console.log(res.data.title);
-      setAnimeList({
-        // added fallback measures to make sure that the content loads even with
-        // incomplete json data to avoid the content not loading entirely
-        name: res?.data?.title_english ?? res?.data?.title,
-        id: res?.data?.mal_id,
-        userRating: 0,
-        image: res?.data?.images?.webp?.image_url,
-        episodes: res?.data?.episodes,
-        currEpisode: 0,
-      });
+      const { data, error } = await supabase
+        .from("user_library")
+        .select("*")
+        .eq("user_id", userId);
+
+      if (error) {
+        console.log(error.message);
+      } else {
+        setAnimeList(data);
+      }
     };
     fetch_data();
   }, []);
@@ -47,7 +45,11 @@ export default function Library() {
       <Header />
       <main>
         <section className="library-shows">
-          {animeList ? <LibraryCard {...animeList} /> : <h1>no records</h1>}
+          {animeList?.map((data) => {
+            return (
+              <LibraryCard {...data} name={data.title} image={data.thumbnail} />
+            );
+          })}
         </section>
       </main>
     </>
