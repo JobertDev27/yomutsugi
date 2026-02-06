7de9b2897eaf040f1988e2b420e3c8e0fe65fd16
Jobert Poylan Jr <jobertdev27@gmail.com>
2026-02-06 22:04:32 +0800

    feat: added checking if already listed and conditional rendering of button

--- frontend/src/FullContent.tsx
+++ frontend/src/FullContent.tsx
@@ -1,11 +1,14 @@
 import { useParams } from "react-router";
 import "./fullContent.css";
-import { useEffect, useState } from "react";
+import { useEffect, useState, useContext } from "react";
 import { get_anime_by_id } from "./utils/api";
 import Header from "./components/Header";
+import { sessionContext } from "./utils/SessionProvider";
+import supabase from "./utils/supabase";
 
 interface contentProp {
   title: string;
+  mal_id: string;
   synopsis: string;
   rank: number;
   score: number;
@@ -24,14 +27,18 @@ interface contentProp {
 }
 
 export default function FullContent() {
-  const [data, setData] = useState<contentProp | null>();
+  const [apiData, setApiData] = useState<contentProp | null>();
+  const [isListed, setIsListed] = useState<boolean>(false);
+
+  const userId = useContext(sessionContext)?.user?.id;
+
   let param = useParams();
 
   useEffect(() => {
     const getData = async () => {
       const res = await get_anime_by_id(param.malId!);
       console.log(res);
-      setData({
+      setApiData({
         ...res.data,
         img: res.data.images.webp.large_image_url,
         schedule: res.data.broadcast.string,
@@ -45,40 +52,63 @@ export default function FullContent() {
         ].map((f) => f.name),
       });
     };
+
     getData();
   }, []);
+
+  useEffect(() => {
+    if (!userId || !apiData) return;
+    const checkInLibrary = async () => {
+      const { data, error } = await supabase
+        .from("user_library")
+        .select("*")
+        .eq("user_id", userId)
+        .eq("mal_id", apiData?.mal_id);
+      if (error) {
+        console.log(error);
+      } else {
+        console.log("passed: " + JSON.stringify(data));
+        setIsListed(true);
+      }
+    };
+    checkInLibrary();
+  }, [userId, apiData]);
 
   return (
     <>
       <Header />
       <main className="content-main">
         <section className="content-section">
-          <img src={data?.img} alt="cover image" />
-          <button>ADD TO LIBRARY</button>
+          <img src={apiData?.img} alt="cover image" />
+          {isListed ? (
+            <button>REMOVE FROM LIBRARY</button>
+          ) : (
+            <button>ADD TO LIBRARY</button>
+          )}
         </section>
         <section className="content-section-metadata">
           <div className="content-seperator">
-            <h1>{data?.title}</h1>
+            <h1>{apiData?.title}</h1>
           </div>
           <div className="content-seperator">
             {" "}
-            <p>{data?.synopsis}</p>
+            <p>{apiData?.synopsis}</p>
           </div>
           <div className="content-seperator">
-            <p>Ranking: {data?.rank}</p>
-            <p>Rating: {data?.score}</p>
-            <p>Genre: {data?.genre.join(", ")}</p>
-            <p>Classification: {data?.rating}</p>
+            <p>Ranking: {apiData?.rank}</p>
+            <p>Rating: {apiData?.score}</p>
+            <p>Genre: {apiData?.genre.join(", ")}</p>
+            <p>Classification: {apiData?.rating}</p>
           </div>
-          <p>Episodes: {data?.episodes}</p>
-          <p>Duration: {data?.duration}</p>
-          <p>Date Aired: {data?.aired}</p>
-          {data?.schedule && <p>Release Schedule: {data?.schedule}</p>}
-          <p>Show Type: {data?.type}</p>
-          <p>Studio: {data?.studio}</p>
-          <p>Status: {data?.status}</p>
+          <p>Episodes: {apiData?.episodes}</p>
+          <p>Duration: {apiData?.duration}</p>
+          <p>Date Aired: {apiData?.aired}</p>
+          {apiData?.schedule && <p>Release Schedule: {apiData?.schedule}</p>}
+          <p>Show Type: {apiData?.type}</p>
+          <p>Studio: {apiData?.studio}</p>
+          <p>Status: {apiData?.status}</p>
           <p>
-            Season Aired: {data?.season} {data?.year}
+            Season Aired: {apiData?.season} {apiData?.year}
           </p>
         </section>
       </main>
