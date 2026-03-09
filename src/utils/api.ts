0b87c6a1fc05f769e40e865def930a609f0484c3
Jobert Poylan Jr <jobertdev27@gmail.com>
2026-03-09 16:32:37 +0800

    feat: added endpoint for banner

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,6 +1,6 @@
 async function jikan(query: string) {
-  const api = "https://api.jikan.moe/v4/";
-  const res = await fetch(api + query);
+  const url = "https://api.jikan.moe/v4/";
+  const res = await fetch(url + query);
 
   if (!res.ok) {
     return res.statusText;
@@ -10,6 +10,7 @@ async function jikan(query: string) {
 }
 
 export const get_anime = () => jikan("top/anime");
+export const get_anime_banner = () => jikan("top/anime?filter=airing&limit=10");
 export const get_anime_by_query = (query: string) => jikan(`anime?q=${query}`);
 export const get_curr_season = () => jikan("seasons/now");
 export const get_anime_by_id = (id: string) => jikan(`anime/${id}`);
