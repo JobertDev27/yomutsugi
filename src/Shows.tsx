89e4a286c3721fe8c6f34415bbcb9057d6d84f98
Jobert Poylan Jr <jobertdev27@gmail.com>
2026-03-09 17:14:53 +0800

    feat: changed shows layout

--- src/Shows.tsx
+++ src/Shows.tsx
@@ -1,23 +1,16 @@
 import { useEffect, useState } from "react";
 import { useSearchParams } from "react-router";
-import { get_anime, get_anime_by_query } from "./utils/api";
+import { get_anime, get_anime_banner, get_anime_by_query } from "./utils/api";
 import ShowCard from "./components/ShowCard";
 import Header from "./components/Header";
 import "./styles/shows.css";
+import BannerCarousel from "./components/BannerCarousel";
 
-interface CardProp {
-  name: string;
-  id: number;
-  rating: number;
-  ranking: number;
-  image: string;
-  genre: string[];
-  data: any;
-  episodes: number;
-}
+import { type CardProp } from "./utils/types";
 
 export default function Shows() {
   const [shows, setShows] = useState<CardProp[] | null>(null);
+  const [bannerShows, setBannerShows] = useState<CardProp[]>([]);
 
   const [queryParams, setQueryParams] = useSearchParams();
 
@@ -30,93 +23,112 @@ export default function Shows() {
   // const [startDate, setStartDate] = useState<string>("");
   // const [endDate, setEndDate] = useState<string>("");
 
+  const parseData = (res: any) => {
+    const bannerData: CardProp[] = res.data.map((d: any) => ({
+      name: d?.title_english ?? d?.title,
+      id: d?.mal_id,
+      rating: d?.score,
+      ranking: d?.rank,
+      image: d?.images?.webp?.image_url,
+      episodes: d?.episodes,
+    }));
+    return bannerData;
+  };
+
   useEffect(() => {
-    const fetch_shows = async () => {
+    const fetchBanner = async () => {
+      const res = await get_anime_banner();
+      setBannerShows(parseData(res));
+    };
+    fetchBanner();
+  }, []);
+
+  useEffect(() => {
+    const fetchShows = async () => {
       const q = queryParams.get("q");
       const res = queryParams.get("q")
         ? await get_anime_by_query(q ?? "")
         : await get_anime();
-      const shows_data: CardProp[] = res.data.map((d: any) => ({
-        name: d?.title_english ?? d?.title,
-        id: d?.mal_id,
-        rating: d?.score,
-        ranking: d?.rank,
-        image: d?.images?.webp?.image_url,
-        episodes: d?.episodes,
-      }));
-      setShows(shows_data);
+
+      setShows(parseData(res));
     };
-    fetch_shows();
+    fetchShows();
   }, [queryParams]);
   return (
     <>
       <Header />
       <main className="shows-main">
-        <aside>
-          <form
-            className="search-query"
-            onSubmit={(e) => {
-              e.preventDefault();
-              const form = new FormData(e.currentTarget);
-              setQueryParams({ q: String(form.get("search")) });
-            }}
-          >
-            <div className="query-cont">
-              <label htmlFor="search">Search</label>
-              <input
-                type="search"
-                name="search"
-                onChange={(e) => setQuery(e.target.value)}
-                value={query}
-              />
-            </div>
-            <div className="rating-cont">
-              <div className="rating-sub-cont">
-                <label htmlFor="rating">Rating</label>
-                <input type="text" name="rating" />
+        <div>
+          <h2>TOP SHOWS CURRENTLY AIRING</h2>
+          <BannerCarousel data={bannerShows} />
+        </div>
+        <section className="shows-content">
+          <aside>
+            <form
+              className="search-query"
+              onSubmit={(e) => {
+                e.preventDefault();
+                const form = new FormData(e.currentTarget);
+                setQueryParams({ q: String(form.get("search")) });
+              }}
+            >
+              <div className="query-cont">
+                <label htmlFor="search">Search</label>
+                <input
+                  type="search"
+                  name="search"
+                  onChange={(e) => setQuery(e.target.value)}
+                  value={query}
+                />
               </div>
-              <div className="rating-sub-cont">
-                <label htmlFor="rating-min">Min</label>
-                <input type="text" name="rating-min" />
+              <div className="rating-cont">
+                <div className="rating-sub-cont">
+                  <label htmlFor="rating">Rating</label>
+                  <input type="text" name="rating" />
+                </div>
+                <div className="rating-sub-cont">
+                  <label htmlFor="rating-min">Min</label>
+                  <input type="text" name="rating-min" />
+                </div>
+                <div className="rating-sub-cont">
+                  <label htmlFor="rating-max">Max</label>
+                  <input type="text" name="rating-max" />
+                </div>
               </div>
-              <div className="rating-sub-cont">
-                <label htmlFor="rating-max">Max</label>
-                <input type="text" name="rating-max" />
+              <div className="query-cont">
+                <label htmlFor="">Content Classification</label>
+                <input type="text" />
               </div>
-            </div>
-            <div className="query-cont">
-              <label htmlFor="">Content Classification</label>
-              <input type="text" />
-            </div>
-            <div className="query-cont">
-              <label htmlFor="season">Season</label>
-              <input type="text" />
-            </div>
-            <div className="query-cont">
-              <label htmlFor="year">Year</label>
-              <input type="number" />
-            </div>
-            <button className="search-btn" type="submit">
-              SEARCH
-            </button>
-          </form>
-        </aside>
-        <section className="shows-section">
-          {shows?.map((c, k) => {
-            return (
-              <ShowCard
-                name={c.name}
-                id={c.id}
-                ranking={c.ranking}
-                rating={c.rating}
-                image={c.image}
-                genre={c.genre}
-                user_item={false}
-                episodes={c.episodes}
-                key={k}
-              />
-            );
-          })}
+              <div className="query-cont">
+                <label htmlFor="season">Season</label>
+                <input type="text" />
+              </div>
+              <div className="query-cont">
+                <label htmlFor="year">Year</label>
+                <input type="number" />
+              </div>
+              <button className="search-btn" type="submit">
+                SEARCH
+              </button>
+            </form>
+          </aside>
+          <div className="shows-section">
+            {shows?.map((c, k) => {
+              return (
+                <ShowCard
+                  name={c.name}
+                  id={c.id}
+                  ranking={c.ranking}
+                  rating={c.rating}
+                  image={c.image}
+                  genre={c.genre}
+                  user_item={false}
+                  episodes={c.episodes}
+                  key={k}
+                />
+              );
+            })}
+          </div>
         </section>
       </main>
     </>
