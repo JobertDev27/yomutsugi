967565ea43074226501f063b582a3873fa4191b0
Jobert Poylan Jr <jobertdev27@gmail.com>
2026-03-10 21:50:08 +0800

    feat: implemented carousel function

--- src/components/BannerCarousel.tsx
+++ src/components/BannerCarousel.tsx
@@ -1,35 +1,51 @@
 import "./styles/bannerCarousel.css";
 import { type CardProp } from "../utils/types";
+import { useState } from "react";
 
 type CarouselProp = {
   data: CardProp[];
 };
 
+function BannerData(data: CardProp) {
+  return (
+    <>
+      <img src={data?.image} alt="" />
+      <div className="carousel-data">
+        <p className="">{data?.name}</p>
+        <p>#{data?.ranking}</p>
+        <div className="carousel-desc">
+          <p>{data?.synopsis}</p>
+        </div>
+      </div>
+    </>
+  );
+}
+
 export default function BannerCarousel({ data }: CarouselProp) {
+  const [currBanner, setCurrBanner] = useState<number>(0);
+
+  const moveBanner = (dir: string) => {
+    const step = dir === "right" ? 1 : -1;
+    setCurrBanner((prev) => (prev + step + data.length) % data.length);
+  };
+
   return (
     <div className="banner-carousel">
+      <button className="carousel-prev-btn" onClick={() => moveBanner("left")}>
+        <img src="/previous.png" alt="<" />
+      </button>
+      <button className="carousel-next-btn" onClick={() => moveBanner("right")}>
+        <img src="/next.png" alt="<" />
+      </button>
       <div className="left-carousel-cont">
-        {" "}
-        <img src={data[0]?.image} alt="" />
-        <div className="carousel-data">
-          <p>{data[0]?.name}</p>
-          <p>#{data[0]?.ranking}</p>
-        </div>
+        <BannerData {...data[currBanner]} />
       </div>
+
       <div className="main-carousel-cont">
-        <img src={data[0]?.image} alt="" />
-        <div className="carousel-data">
-          <p>{data[0]?.name}</p>
-          <p>#{data[0]?.ranking}</p>
-        </div>
+        <BannerData {...data[currBanner]} />
       </div>
       <div className="right-carousel-cont">
-        {" "}
-        <img src={data[0]?.image} alt="" />
-        <div className="carousel-data">
-          <p>{data[0]?.name}</p>
-          <p>#{data[0]?.ranking}</p>
-        </div>
+        <BannerData {...data[currBanner]} />
       </div>
     </div>
   );
