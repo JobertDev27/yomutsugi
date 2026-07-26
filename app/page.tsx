b2164baceb70a496be7a724af489b43bfba8e552
jobertdev27 <jobertdev27@gmail.com>
2026-07-26 16:51:07 +0800

    style: fixed based layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,9 @@
+import PageHeader from "@/components/PageHeader";
+
 export default function Home() {
-  return <h1>Hello World disregard</h1>;
+  return (
+    <>
+      <PageHeader />
+    </>
+  );
 }
