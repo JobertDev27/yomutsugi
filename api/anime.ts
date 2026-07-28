20abd8a81b3faa5a7df00115921c09a514794532
jobertdev27 <jobertdev27@gmail.com>
2026-07-28 17:39:00 +0800

    feat: added api mapper and cleaned up data

--- api/anime.ts
+++ api/anime.ts
@@ -1,12 +1,26 @@
+import { Anime, Show } from "@/type/types";
+
 const url: string = "https://api.tenrai.org/v1/";
+
+function _apiMapper(data: Anime): Show {
+  return {
+    title: data.title,
+    thumbnail: data.images.webp.small_image_url,
+    cover: data.images.webp.large_image_url,
+    episodes: data.episodes
+  };
+}
 
 async function getAllAnime() {
   const res = await fetch(`${url}anime`);
-  return await res.json();
+  const json = await res.json();
+  return json.data.map(_apiMapper);
 }
 
 async function searchBy(title?: string) {
   const res = await fetch(`${url}anime?q=${title}`);
+  const data = await res.json();
+  return _apiMapper(data);
 }
 
 async function getAnimeById(id: number) {
