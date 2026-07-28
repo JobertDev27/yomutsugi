eb3e7558b114a4b9120bf179b38fd92a98c88031
jobertdev27 <jobertdev27@gmail.com>
2026-07-28 17:41:17 +0800

    feat: changed type to show

--- components/ShowCard.tsx
+++ components/ShowCard.tsx
@@ -1,10 +1,10 @@
-import { Anime } from "@/type/types";
+import { Show } from "@/type/types";
 import Image from "next/image";
 
-export default function ShowCard(show: Anime) {
+export default function ShowCard(show: Show) {
   return (
     <div>
-      <Image src={show.images.webp.image_url} width={405} height={720} alt="" />
+      <Image src={show.thumbnail} width={405} height={720} alt="" />
       <p>{show.title}</p>
     </div>
   );
