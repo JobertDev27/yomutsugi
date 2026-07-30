15410e5547018bdfa1ff4ac4d946084bc4d67b78
jobertdev27 <jobertdev27@gmail.com>
2026-07-30 21:49:17 +0800

    feat: added search form

--- components/PageHeader.tsx
+++ components/PageHeader.tsx
@@ -1,12 +1,18 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+
 export default function PageHeader() {
+  const router = useRouter();
   return (
     <header className="w-full flex justify-between p-5 box-border">
       <div>
         <h1>Yomutsugi</h1>
       </div>
-      <div>
-        <search></search>
-      </div>
+      <form action={"/shows"}>
+        <input type="search" name="q" placeholder="search..." />
+        <button type="submit">search</button>
+      </form>
       <nav>
         <p>Login</p>
       </nav>
