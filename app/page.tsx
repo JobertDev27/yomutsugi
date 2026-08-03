0ffe5254975a7f48bf8f7090c46c34a22bbbcde2
jobertdev27 <jobertdev27@gmail.com>
2026-08-03 21:35:12 +0800

    feat: added login text

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,9 @@ export default function Home() {
   return (
     <>
       <PageHeader />
+      <main className="flex justify-center items-center flex-1">
+        <p>Please Login to see Saved Library</p>
+      </main>
     </>
   );
 }
