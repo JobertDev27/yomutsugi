752cf87384bab4943c829282826a3ccaac4242ba
jobertdev27 <jobertdev27@gmail.com>
2026-08-04 23:32:28 +0800

    feat: search query functionality

--- app/shows/page.tsx
+++ app/shows/page.tsx
@@ -4,21 +4,24 @@ import ShowCard from "@/components/ShowCard";
 import { useState, useEffect } from "react";
 import { useSearchParams } from "next/navigation";
 import PageHeader from "@/components/PageHeader";
-import { getAllAnime } from "@/api/anime";
+import { getAllAnime, getAnimeByName } from "@/api/anime";
 import { Show } from "@/type/types";
 
 export default function Shows() {
   const [shows, setShows] = useState<Show[]>([]);
 
   const search = useSearchParams();
+
   console.log(search);
 
   useEffect(() => {
     const fetchShows = async () => {
       try {
-        const tempShows = await getAllAnime();
-        setShows(tempShows);
+        const tempShows = search.get("q")
+          ? await getAnimeByName(search.get("q") || "")
+          : await getAllAnime();
         console.log("successful:", tempShows);
+        setShows(tempShows);
       } catch (err) {
         console.error(err);
       }
@@ -30,7 +33,6 @@ export default function Shows() {
     <>
       <PageHeader />
       <main>
-        <p>{search.get("q")}</p>
         <section className="grid lg:grid-cols-8 md:grid-cols-6 grid-cols-4 gap-4 p-4">
           {shows?.map((show, i) => (
             <ShowCard {...show} key={i} />
