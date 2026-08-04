188b6a62ef5c28acd25acebf52713e34dc0f5ff3
jobertdev27 <jobertdev27@gmail.com>
2026-08-04 23:16:10 +0800

    feat: used useSearchParams to get url query

--- app/shows/page.tsx
+++ app/shows/page.tsx
@@ -1,13 +1,18 @@
 "use client";
 
-import { useEffect, useState } from "react";
-import { getAllAnime } from "@/api/anime";
 import ShowCard from "@/components/ShowCard";
-import { Show } from "@/type/types";
+import { useState, useEffect } from "react";
+import { useSearchParams } from "next/navigation";
 import PageHeader from "@/components/PageHeader";
+import { getAllAnime } from "@/api/anime";
+import { Show } from "@/type/types";
 
 export default function Shows() {
   const [shows, setShows] = useState<Show[]>([]);
+
+  const search = useSearchParams();
+  console.log(search);
+
   useEffect(() => {
     const fetchShows = async () => {
       try {
@@ -20,10 +25,12 @@ export default function Shows() {
     };
     fetchShows();
   }, []);
+
   return (
     <>
       <PageHeader />
       <main>
+        <p>{search.get("q")}</p>
         <section className="grid lg:grid-cols-8 md:grid-cols-6 grid-cols-4 gap-4 p-4">
           {shows?.map((show, i) => (
             <ShowCard {...show} key={i} />
