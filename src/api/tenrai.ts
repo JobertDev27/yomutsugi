85ee9823930faff4357fef0c7abd7d2ad5f80180
jobertdev27 <jobertdev27@gmail.com>
2026-08-15 22:35:41 +0800

    feat: added tenrai api

---
+++ src/api/tenrai.ts
@@ -0,0 +1,23 @@
+import type { Show, TenraiApi } from "../types/type";
+
+const url: string = "https://api.tenrai.org/v1/";
+
+function _apiMapper(data: TenraiApi): Show {
+  return {
+    title: data.title,
+    thumbnail: data.images.webp.small_image_url,
+    cover: data.images.webp.large_image_url,
+  };
+}
+
+export async function getAllAnime(): Promise<Show> {
+  const res = await fetch(`${url}anime`);
+  const json = await res.json();
+  return json.data.map(_apiMapper);
+}
+
+export async function searchBy(title?: string): Promise<Show> {
+  const res = await fetch(`${url}anime?q=${title}`);
+  const data = await res.json();
+  return _apiMapper(data);
+}
