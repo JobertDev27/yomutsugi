9995821e4b16acf27f42024c6907723493c3f5e5
jobertdev27 <jobertdev27@gmail.com>
2026-08-18 15:33:39 +0800

    style: added grid layout with responsive layout

--- src/routes/animes.tsx
+++ src/routes/animes.tsx
@@ -25,7 +25,7 @@ function Animes():React.ReactNode {
     return (
 	<>
 	<Header />
-	<main>
+	<main className="grid lg:grid-cols-6 md:grid-cols-4 grid-cols-2  gap-2">
 	{shows.map((s, key) => <ShowCard show={s} key={key} />)}	    
 	</main>	
 	</>
