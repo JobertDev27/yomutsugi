c866f35f640488980265302179dc5d607e738c1f
jobertdev27 <jobertdev27@gmail.com>
2026-08-22 18:26:12 +0800

    feat: added supabase claims to check session

--- src/routes/animes.index.tsx
+++ src/routes/animes.index.tsx
@@ -1,33 +1,56 @@
 import { createFileRoute } from "@tanstack/react-router";
-import Header from "../components/Header";
 import { useEffect, useState } from "react";
-import type { Show } from "../types/type";
-import { getAllAnime } from "../api/tenrai";
-import ShowCard from "../components/ShowCard";
+import { supabase } from "../utils/supabase";
+import type { JwtPayload } from "@supabase/supabase-js";
 
 export const Route = createFileRoute('/animes/')({
     component: Animes,
 })
 
 function Animes():React.ReactNode {
-    const [shows, setShows] = useState<Show[]>([])
-    
+    const [claims, setClaims] = useState<JwtPayload | null>(null)
+    //const [shows, setShows] = useState<Show[]>([])
+
     useEffect(() => {
-	const fetchShows = async () => {
-	    const temp = await getAllAnime()
-	    console.log(temp)
-	    setShows(temp)
+	const params = new URLSearchParams(window.location.search);
+	const token_hash = params.get("token_hash");
+	const type = params.get("type");
+
+	if (token_hash) {
+	    supabase.auth.verifyOtp({
+		token_hash,
+		type: type || "email",
+	    });
 	}
-	fetchShows()
 
-    },[])
+	supabase.auth.getClaims().then(({ data }) => {
+	    setClaims(data?.claims ?? null)
+	});
 
+	const {
+	    data: { subscription },
+	} = supabase.auth.onAuthStateChange(() => {
+	    supabase.auth.getClaims().then(({ data }) => {
+		setClaims(data?.claims ?? null);
+	    });
+	});
+
+	return () => subscription.unsubscribe();
+    }, []);
+
+    // If user is logged in, show welcome screen
+    if (claims) {
+	return (
+	    <div>
+	    <h1>Welcome!</h1>
+	    <p>You are logged in as: {claims.email}</p>
+	    </div>
+	)
+    }
+    // Show login form
     return (
-	<>
-	<Header />
-	<main className="grid lg:grid-cols-8 md:grid-cols-4 grid-cols-2 gap-3">
-	{shows.map((s, key) => <ShowCard show={s} key={key} />)}	    
-	</main>	
-	</>
+	<div>
+	<p>Hello World</p>
+	</div>
     )
 }
