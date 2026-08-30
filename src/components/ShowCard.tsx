55033e7f4cf943b0166a65624f137bed23f9160d
jobertdev27 <jobertdev27@gmail.com>
2026-08-30 19:07:46 +0800

    feat: made text inside thumbnail to save space

--- src/components/ShowCard.tsx
+++ src/components/ShowCard.tsx
@@ -7,10 +7,10 @@ type ShowProps = {
 
 export default function ShowCard({show}: ShowProps): React.ReactNode {
     return <Link to="/animes/$animeId" params={{animeId: String(show.id)}}>
-	<div>
+	<div className="relative">
 	    <img className="max-h-[250px] w-full  min-h-25" src={show.cover} alt={`${show.title} cover`} />
-	    <div>
-	    <p>{show.title}</p> 
+	    <div className="absolute bottom-0 left-0 p-2 w-full bg-linear-to-t from-[rgba(0,0,0,0.8)] backdrop-blur-sm">
+	    <p className="font-bold">{show.title}</p> 
 	    </div>
 	</div>
     </Link>
