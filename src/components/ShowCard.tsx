f6a5bc91f459ee7d0262919379cad80430c7205e
jobertdev27 <jobertdev27@gmail.com>
2026-09-01 19:23:32 +0800

    style: lenghtened cards

--- src/components/ShowCard.tsx
+++ src/components/ShowCard.tsx
@@ -8,7 +8,7 @@ type ShowProps = {
 export default function ShowCard({show}: ShowProps): React.ReactNode {
     return <Link to="/animes/$animeId" params={{animeId: String(show.id)}}>
 	<div className="relative">
-	    <img className="max-h-[250px] w-full  min-h-25" src={show.cover} alt={`${show.title} cover`} />
+	    <img className="max-h-[280px] w-full  min-h-25" src={show.cover} alt={`${show.title} cover`} />
 	    <div className="absolute bottom-0 left-0 p-2 w-full bg-linear-to-t from-[rgba(0,0,0,0.8)] backdrop-blur-sm">
 	    <p className="font-bold">{show.title}</p> 
 	    </div>
