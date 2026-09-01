1228fe09c79dac6f5adbcf186ce818a1994c106a
jobertdev27 <jobertdev27@gmail.com>
2026-09-01 19:34:15 +0800

    feat: added type for desc and synopsis

--- src/types/type.ts
+++ src/types/type.ts
@@ -4,17 +4,19 @@ export type Show = {
     cover: string;
     thumbnail: string;
     ep: number;
+    desc: string;
 }
 
 export type Tenrai = {
-  mal_id: number;
-  title: string;
-  episodes: number;
-  images: {
-    webp: {
-      image_url: string;
-      large_image_url: string;
-      small_image_url: string;
+    mal_id: number;
+    title: string;
+    episodes: number;
+    synopsis: string;
+    images: {
+	webp: {
+	    image_url: string;
+	    large_image_url: string;
+	    small_image_url: string;
+	};
     };
-  };
 };
