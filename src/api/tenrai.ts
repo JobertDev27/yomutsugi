2e622a6e22c8636e4f78432c26a8496919196f6c
jobertdev27 <jobertdev27@gmail.com>
2026-09-03 19:50:51 +0800

    feat: added more data to fetch from api

--- src/api/tenrai.ts
+++ src/api/tenrai.ts
@@ -3,31 +3,41 @@ import type { Show, Tenrai } from "../types/type";
 const url: string = "https://api.tenrai.org/v1/";
 
 function _apiMapper(data: Tenrai): Show {
-  return {
-    id: data.mal_id,
-    title: data.title,
-    thumbnail: data.images.webp.small_image_url,
-    cover: data.images.webp.large_image_url,
-    ep: data.episodes,
-    desc: data.synopsis,
-  };
+    return {
+	id: data.mal_id,
+	title: data.title,
+	thumbnail: data.images.webp.small_image_url,
+	cover: data.images.webp.large_image_url,
+	ep: data.episodes,
+	status: data.status,
+	aired: data.aired.string,
+	duration: data.duration,
+	rating: data.rating,
+	score: data.score,
+	rank: data.rank,
+	season: data.season,
+	year: data.year,
+	//genres: data.genres[],
+	//themes: data.themes[],
+	desc: data.synopsis,
+    };
 }
 
 export async function getAllAnime(): Promise<Show[]> {
-  const res = await fetch(`${url}anime`);
-  const json = await res.json();
-  return json.data.map(_apiMapper);
+    const res = await fetch(`${url}anime`);
+    const json = await res.json();
+    return json.data.map(_apiMapper);
 }
 
 export async function getAnimeByTitle(title?: string): Promise<Show> {
-  const res = await fetch(`${url}anime?q=${title}`);
-  const json = await res.json();
-  return _apiMapper(json.data);
+    const res = await fetch(`${url}anime?q=${title}`);
+    const json = await res.json();
+    return _apiMapper(json.data);
 }
 
 
 export async function getAnimeById(id: string): Promise<Show> {
-  const res = await fetch(`${url}anime/${id}`);
-  const json = await res.json();
-  return _apiMapper(json.data);
+    const res = await fetch(`${url}anime/${id}`);
+    const json = await res.json();
+    return _apiMapper(json.data);
 }
