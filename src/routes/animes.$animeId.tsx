cf2282aead2a3f4e5c77c6f29d5392bc68ec2389
jobertdev27 <jobertdev27@gmail.com>
2026-09-07 22:19:48 +0800

    style: made style more good looking

--- src/routes/animes.$animeId.tsx
+++ src/routes/animes.$animeId.tsx
@@ -3,6 +3,9 @@ import { getAnimeById } from '../api/tenrai'
 import type { Show } from '../types/type'
 import {useEffect, useRef, useState} from "react"
 
+import starIcon from '../assets/star.png'
+import userIcon from '../assets/user.png'
+
 export const Route = createFileRoute('/animes/$animeId')({
     loader: async ({ params }) => {
 	console.log('LOADER RAN', params.animeId)
@@ -27,13 +30,23 @@ function RouteComponent() {
     }, [])
 
     return <main className='flex flex-col flex-1 justify-center items-center pt-10 mt-10'>
-    <section className='flex flex-col justify-center items-center border-border border-2 max-w-[1000px] p-5 mt-10! gap-2'>
+    <section className='flex flex-col justify-center border-border border-2 max-w-[1000px] p-5 mt-10! gap-2'>
+    <div className='w-full flex mb-4! justify-center items-center'>
     <img src={anime.cover} className='max-h-100' />
-    <div className='w-full'>
-    <h1 className='font-bold text-xl'>{anime.title}</h1>
     </div>
+    <h1 className='font-bold text-xl'>{anime.title}</h1>
+    <p> <span className='font-bold'>Ranked:</span> #{anime.rank}</p>
+
+    <div className='flex items-center gap-2'>
+    <p className='font-bold text-md'>Ratings: {anime.score}</p>
+    <img className='w-[1rem]' src={starIcon} />
+    <p>({anime.scored_by.toLocaleString()}</p>
+    <img className='w-[1rem]' src={userIcon} />
+    <p>)</p>
+    </div>
+    <button className='bg-prim-text text-bg! max-w-fit rounded-2xl px-5 py-1 my-2!'>Add To Library</button>
     <p>{anime.ep}</p>
-    <div>
+    <h2 className='font-bold text-lg'>Synopsis</h2>
     <p
     ref={textRef}
     className={expanded ? "" : "max-h-24 overflow-hidden"}
@@ -42,18 +55,16 @@ function RouteComponent() {
     </p>
 
     {isOverflowing && (
-	<button className='text-blue-200! cursor-pointer' onClick={() => setExpanded(prev => !prev)}>
+	<button className='text-blue-200! max-w-fit cursor-pointer' onClick={() => setExpanded(prev => !prev)}>
 	{expanded ? "Read less" : "Read more"}
 	</button>
     )}
-    </div>
-    <p>{anime.status}</p>
-    <p>{anime.aired}</p>
-    <p>{anime.rank}</p>
-    <p>{anime.rating}</p>
-    <p>{anime.duration}</p>
-    <p>{anime.year}</p>
-    <p>{anime.score}</p>
+    <h2 className='font-bold'>More Information</h2>
+    <p> <span className='font-bold'>Status:</span> {anime.status}</p>
+    <p> <span className='font-bold'>Aired:</span> {anime.aired}</p>
+    <p> <span className='font-bold'>Show Rating:</span> {anime.rating}</p>
+    <p> <span className='font-bold'>Duration:</span> {anime.duration}</p>
+    <p> <span className='font-bold'>Year Released:</span> {anime.year}</p>
     </section>
     </main>
 
